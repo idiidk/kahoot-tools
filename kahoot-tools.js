682f164c5eb8378b144975fc13789ea637e56e60
idiidk <dviide@protonmail.com>
2018-04-28 16:04:48 +0200

    Defaulting to port 80

--- kahoot-tools.js
+++ kahoot-tools.js
@@ -1,26 +1,26 @@
-const fs = require('fs');
+const fs = require("fs");
 
-if (!fs.existsSync('./dist')) {
-    console.log('Error, please run: `npm run build` before launching the server!')
+if (!fs.existsSync("./dist")) {
+    console.log("Error, please run: `npm run build` before launching the server!")
 } else {
-    const corsAnywhere = require('cors-anywhere');
-    const express = require('express');
+    const corsAnywhere = require("cors-anywhere");
+    const express = require("express");
     const app = express();
 
-    const host = '0.0.0.0';
-    const appPort = 8080;
+    const host = "0.0.0.0";
+    const appPort = 80;
     const corsPort = 3000;
 
-    app.use(express.static('./dist'))
+    app.use(express.static("./dist"))
 
     app.listen(appPort, () => {
-        console.log('Running server on port ' + appPort);
+        console.log("Running server on port " + appPort);
     });
 
     corsAnywhere.createServer({
         originWhitelist: [], // Allow all origins
-        requireHeader: ['origin', 'x-requested-with'],
+        requireHeader: ["origin", "x-requested-with"],
     }).listen(corsPort, host, function () {
-        console.log('Running CORS Anywhere on port ' + corsPort);
+        console.log("Running CORS Anywhere on port " + corsPort);
     });
 }
