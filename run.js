2fb0082ad0db0587195e7013d17e552132ab1335
idiidk <dviide@protonmail.com>
2019-09-21 21:57:27 +0200

    Fixed run script & bumped version

--- run.js
+++ run.js
@@ -1,11 +1,23 @@
+const handler = require("serve-handler");
+const http = require("http");
 const config = require("./config");
 
-const host = process.env.HOST || "0.0.0.0";
-const port = process.env.PORT || config.corsPort;
+const host = process.env.HOST || config.host;
+const webPort = process.env.WEB_PORT || config.webPort;
+const corsPort = process.env.CORS_PORT || config.corsPort;
 
-const cors_proxy = require("cors-anywhere");
-cors_proxy.createServer({
-  originWhitelist: [],
-}).listen(port, host, function () {
-  console.log("Running CORS Anywhere on " + host + ":" + port);
+const corsProxy = require("cors-anywhere");
+const corsServer = corsProxy.createServer({
+  originWhitelist: []
+});
+
+const webServer = http.createServer((request, response) => {
+  return handler(request, response, { public: "./dist/" });
+});
+
+corsServer.listen(corsPort, host, function() {
+  console.log(`[+] CORS anywhere running on port ${corsPort}`);
+});
+webServer.listen(webPort, host, () => {
+  console.log(`[+] Serving site at http://${host}:${webPort}`);
 });
